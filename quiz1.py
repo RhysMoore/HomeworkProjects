4e32a2207f28e1376cdd60b0ce783c1430cc97ae
Rhys Moore <Rhys.moore@vtsd.com>
2021-09-27 13:02:19 -0400

    Done

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,15 @@
-Put some stuff here for my code
+number1 = input("What is the first number you would like to pick?")
+number2 = input("What is the second number you would like to pick?")
+
+sum = int(number1) + int(number2)
+
+prod = int(number1) * int(number2)
+
+while (sum != prod):
+    print("Really? You thought that would work? Try again .")
+    number1 = input("First number?")
+    number2 = input("Second number?")
+    sum = int(number1) + int(number2)
+    prod = int(number1) * int(number2)
+
+print("You did it, the sum and product are equal... What exactly did you gain again?")
